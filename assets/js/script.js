14f5f617a254c567b213ebbd002dc53fe716ce98
CPMSP <cpetersonmsp@gmail.com>
2021-01-06 09:29:00 -0600

    jQuery ToDo list complete

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// Check off list items by clicking
+
+$('ul').on('click', 'li', function () {
+    $(this).toggleClass('completed');
+});
+
+// Click on "X" to remove To Do
+
+$('ul').on('click', 'span', function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        // grab input text
+        let todoText = $(this).val();
+        $(this).val("");
+        // create a new li and add to ul
+        $('ul').append("<li><span><i class = 'fa fa-trash'></i></span> " + todoText + "</li>");
+    };
+});
+
+$('.fa-plus-square').click(function (){
+    $("input[type=text]").fadeToggle();
+});
